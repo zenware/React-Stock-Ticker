68572e31fc07b8f9302d81e14a252e9bdb5c967e
RecursiveLogic <qrx9823@gmail.com>
2016-01-21 01:39:37 -0500

    Fixed state scoping issue

--- client/containers/App.js
+++ client/containers/App.js
@@ -3,6 +3,10 @@ import request from 'superagent'
 import DisplayField from './DisplayField'
 
 module.exports = React.createClass({
+    componentDidMount: function() {
+        var ping = Math.floor(Math.random() * 7000);
+        setTimeout(this.updateStocks, ping);
+    },
     getInitialState: function() {
         return {
             symbol: '',
@@ -18,6 +22,8 @@ module.exports = React.createClass({
         var queryURL = 'https://finance.google.com/finance/info?client=ig&q=';
         var symbol = this.state.symbol.toUpperCase();
         var stocks = this.state.stocks;
+        var ping = Math.floor(Math.random() * 7000);
+        var self = this;
         e.preventDefault();
         request
             .get(queryURL + symbol)
@@ -27,11 +33,32 @@ module.exports = React.createClass({
                     var a = res.text.split('');
                     var data = JSON.parse(a.slice(6, a.length - 2).join(''));
                     stocks.push(data);
+                    self.setState({
+                        symbol: '',
+                        stocks: stocks
+                    });
                 }
             });
+
+    },
+    updateStocks: function() {
+        var queryURL = 'https://finance.google.com/finance/info?client=ig&q=';
+        var stocks = this.state.stocks;
+        var updatedStocks = [];
+        stock.forEach(function(x) {
+            request
+                .get(queryURL + x.t)
+                .end(function(err, res) {
+                    if (err) { console.log(err); }
+                    else {
+                        var a = res.text.split('');
+                        var data = JSON.parse(a.slice(6, a.length - 2).join(''));
+                        updatedStocks.push(data);
+                    }
+                });
+        });
         this.setState({
-            symbol: '',
-            stocks: stocks
+            stocks: updatedStocks
         });
     },
     render: function() {
