85c1da2e26054147933b174b4963caee414d034c
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-14 09:02:40 -0600

    none of the attempted configuration worked, clear it

--- server.js
+++ server.js
@@ -6,24 +6,6 @@ var app = express();
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'client');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.configure('production', function () {
-    app.use(allowCrossDomain);
-});
-
 app.use(express.static(static_path))
     .get('/', function (req, res) {
         res.sendFile('index.html', {
