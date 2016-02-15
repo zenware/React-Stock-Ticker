4eb8316c39872f6cb38f21da71c4cd2efbbabb3e
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-15 00:36:31 -0600

    sourcemaps arent necessary in production

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -9,7 +9,6 @@ module.exports = {
   output: {
     path: path.join(__dirname, './client'),
     filename: 'bundle.js',
-    sourceMapFilename: 'bundle.js.map',
     publicPath: '/client/'
   },
   plugins: [
