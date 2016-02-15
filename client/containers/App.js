6a05a7b83566de527aa13b1852fd315b91e51f4b
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-14 20:20:33 -0600

    Actually Use the API Endpoint

--- client/containers/App.js
+++ client/containers/App.js
@@ -8,12 +8,6 @@ module.exports = React.createClass({
         var ping = Math.floor(Math.random() * 10000);
         setInterval(this.updateStocks, (ping > 5000 ? ping : 7500));
     },
-    getInitialState() {
-        return {
-            symbol: '',
-            stocks: []
-        };
-    },
     handleChange(e) {
         this.setState({
             symbol: e.target.value
@@ -25,25 +19,29 @@ module.exports = React.createClass({
         var symbol = this.state.symbol.toUpperCase();
         var stocks = this.state.stocks;
         var self = this;
+
         if (!this.validateInput(symbol)) {
             alert("It already exists!");
             return this.setState({
                 symbol: ''
             });
         }
+
         request
             .get(queryURL + symbol)
+            .set('Accept', 'application/json')
             .end(function(err, res) {
-                if (err) { 
-                    console.log(err); 
-                } 
+                if (err) {
+                    console.log(err);
+                }
                 if (res.status === 400) {
                     alert("Stock symbol doesn't exist, try another one.");
                     self.setState({
                         symbol: ''
                     });
                 } else {
-                    var a = res.text['message'].split('');
+                    console.log(res.text);
+                    var a = res.text['stocks'].split('');
                     var data = a.slice(6, a.length - 2).join('');
                     stocks.unshift(data);
                     if (stocks.length > 6) stocks = stocks.slice(0, 6);
@@ -55,15 +53,7 @@ module.exports = React.createClass({
             });
     },
     formatSymbols(array) {
-        var stocks = "";
-        for (var i = 0; i < array.length; ++i) {
-            if (i == array.length - 1) {
-                stocks += array[i].t;
-                break;
-            }
-            stocks += array[i].t + ',';
-        }
-        return stocks;
+        return array.toString();
     },
     updateStocks() {
         var stocks = this.state.stocks;
@@ -72,10 +62,11 @@ module.exports = React.createClass({
         var self = this;
         request
             .get(queryURL)
+            .set('Accept', 'application/json')
             .end(function(err, res) {
                 if (err) { console.log(err); }
                 else {
-                    var a = res.text['message'].split('');
+                    var a = res.text['stocks'].split('');
                     var data = a.slice(3, a.length).join('');
                     self.setState({
                         stocks: data
@@ -84,13 +75,7 @@ module.exports = React.createClass({
             });
     },
     validateInput(input) {
-        var stocks = this.state.stocks;
-        for (var i = 0; i < stocks.length; ++i) {
-            if (input === stocks[i].t) {
-                return false;
-            }
-        }
-        return true;
+        return this.state.stocks.includes(input);
     },
     createCORSRequest(method, url) {
         var xhr = new XMLHttpRequest();
@@ -105,18 +90,20 @@ module.exports = React.createClass({
         return xhr;
     },
     render: function() {
-        return (<div className="main">
+        return (
+	        <div className="main">
             <div className="box">
-                <form className="container-1" onSubmit={ this.handleSubmit }> 
+                <form className="container-1" onSubmit={ this.handleSubmit }>
                     <span className="search-icon"><i className="fa fa-search"></i></span>
-                    <input id="search" type="text" 
+                    <input id="search" type="text"
                         value={ this.state.symbol } onChange={ this.handleChange }
                         placeholder="Search..." />
                 </form>
             </div>
             <DisplayField stocks={ this.state.stocks } />
             <Footer />
-        </div>);
+          </div>
+	      );
     }
 });
 
