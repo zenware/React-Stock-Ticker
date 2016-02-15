6e9d8c3d80f5896f2d1c35bfcfc77bb67f0b546b
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-15 15:54:54 -0600

    fix some typos

--- client/containers/StockTickerList.js
+++ client/containers/StockTickerList.js
@@ -3,10 +3,10 @@ import StockTicker from './StockTicker'
 
 module.exports = React.createClass({
   render() {
-    return (
-      this.props.tickers.map( x => {
+    return this.props.tickers.map(
+      x => {
         <li key={x} className='list-item'><StockTicker ticker={x}/></li>
-      });
+      }
     );
   }
 });
