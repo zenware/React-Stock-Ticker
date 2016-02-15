dc4a4d9b65381d2f981b8b570e2620d4fdbcc1c5
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-15 15:58:00 -0600

    switch nonexistent variable to real property

--- client/containers/DisplayField.js
+++ client/containers/DisplayField.js
@@ -3,14 +3,13 @@ import StockTickerList from './StockTickerList'
 
 module.exports = React.createClass({
     render() {
-      var tickers = this.props.tickers;
-      if (tickers.length < 1) {
+      if (this.props.tickers.length < 1) {
         return <div className="display-field"></div>;
       }
       return (
         <div className="display-field">
         <ul>
-          { <StockTickerList tickers={x}/> }
+          { <StockTickerList tickers={this.props.tickers}/> }
         </ul>
         </div>
       )
