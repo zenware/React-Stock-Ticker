faa0c48452cd8139eeebd4f3dba22193df0924c5
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-15 02:20:12 -0600

    add a null stock ticker to get appropriate requests

--- client/containers/App.js
+++ client/containers/App.js
@@ -11,7 +11,7 @@ module.exports = React.createClass({
     getInitialState() {
       return {
         search: 'GOOG',
-        tickers: [],
+        tickers: ['null't ],
         stocks: []
       };
     },
